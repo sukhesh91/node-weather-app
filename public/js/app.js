2eb8f50d21c130ffaa204aa08aac3777af136ce5
Sukhesh <sukhesh@apples-MacBook-Pro.local>
2022-01-09 12:21:55 +0530

    Remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js is loaded')
-
 const searchForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg-1')
