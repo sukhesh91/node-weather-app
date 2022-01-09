068877f391b5c44a8e8e8286e54c4e66ea2f56f0
Sukhesh <sukhesh@apples-MacBook-Pro.local>
2022-01-09 23:34:46 +0530

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
     msg1.textContent = 'Loading...'
     msg2.textContent = ''
-    fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+    fetch('/weather?address='+search.value).then((response) => {
     response.json().then((data) => {
         if(data.error){
             msg1.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express configs
 const publicPath = path.join(__dirname, '../public')
@@ -84,6 +85,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server up in port 3000')
+app.listen(port, () => {
+    console.log('Server up in port ' + port)
 })
